fc21ccc5b970f7d41176a60b17aa40deeb19b3c9
ESSO <islamdiab967@gmail.com>
2023-01-09 04:14:24 +0200

    create a count down for lounching soon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown'
+import "@leenguyen/react-flip-clock-countdown/dist/index.css";
+import './style.css';
+
+import VideoBg from "../src/assets/video.mp4";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="page">
+      <div className='overlay'></div>
+      <video src={VideoBg} autoPlay loop muted></video>
+      <div className='page__content'>
+        <h1>Lounching Soon</h1>
+        <h3>Leave your email and we'll let you know once the site goes live.</h3>
+        <FlipClockCountdown
+          to={new Date().getTime() + 24 * 3600 * 1000 + 5000}
+          />
+          <button className='btn'>Notify me</button>
+      </div>
+    </section>
   );
 }
 
